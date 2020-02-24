ed60244de8bddb7f3ef979d59fcf90e74f373d99
PauloJoseBR <paulotec96@gmail.com>
2020-02-24 01:37:27 -0300

    creating dynamic cards

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 4,
+        name: 'Paulo Silva',
+        username: 'paulo.silva',
+        email: 'paulo.tec96@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Regiane',
+        username: 'regiane',
+        email: 'regianee@bol.com'
+    },
+    {
+        id: 5,
+        name: 'Zilu',
+        username: 'ziluzinha',
+        email: 'ziluzinha@bol.com'
+    }
+]
