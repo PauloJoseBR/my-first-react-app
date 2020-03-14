ff457418ec808b663140675aa5e2b7e10ca06ee9
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2020-03-14 20:52:16 -0300

    Updates

---
+++ precache-manifest.baacd332c7e9425ac33fa4f5923c956a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9630756ecc2d828f8195534cfadd8b4e",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "d9e82439880dcff014ce",
+    "url": "/my-first-react-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bdbd2f024df372e25bd6",
+    "url": "/my-first-react-app/static/css/main.831ad4e6.chunk.css"
+  },
+  {
+    "revision": "d9e82439880dcff014ce",
+    "url": "/my-first-react-app/static/js/2.f0797f89.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/my-first-react-app/static/js/2.f0797f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbd2f024df372e25bd6",
+    "url": "/my-first-react-app/static/js/main.212cdc46.chunk.js"
+  },
+  {
+    "revision": "8188148212e22c37f55d",
+    "url": "/my-first-react-app/static/js/runtime-main.e250155e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-first-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
